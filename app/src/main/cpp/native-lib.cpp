b565e3f7a6fcbf9786088f283cb60db93f3ff4f2
chyves <82107846+notchyves@users.noreply.github.com>
2024-12-15 18:12:55 +0000

    input & opengl

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,9 +1,36 @@
 #include <jni.h>
 #include <string>
+#include <GLES2/gl2.h>
+#include <android/log.h>
 
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_chyves_opencraft_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+#define LOG_TAG "OpenCraft"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+
+extern "C" {
+    JNIEXPORT void JNICALL Java_com_chyves_opencraft_MainActivity_handleTouchEventInCpp(JNIEnv *env, jobject, jfloat x, jfloat y, jint action) {
+        LOGD("Touch event: x=%.2f, y=%.2f, action=%d", x, y, action);
+    }
+
+    JNIEXPORT void JNICALL Java_com_chyves_opencraft_MainActivity_handleKeyEventInCpp(JNIEnv *env, jobject, jint keyCode, jstring keyChar) {
+        const char *keyCharCStr = env->GetStringUTFChars(keyChar, nullptr);
+
+        LOGD("Key event: keyCode=%d, keyChar=%s", keyCode, keyCharCStr);
+        env->ReleaseStringUTFChars(keyChar, keyCharCStr);
+
+        // eventually both input handlers will route into main c++ file through event system
+    }
+
+    JNIEXPORT void JNICALL Java_com_chyves_opencraft_MainActivity_nativeInitOpenGL(JNIEnv *env, jobject obj) {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    JNIEXPORT void JNICALL Java_com_chyves_opencraft_MainActivity_nativeRender(JNIEnv *env, jobject obj) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // both renderers will eventually will also route into main c++
+    }
+
+    JNIEXPORT void JNICALL Java_com_chyves_opencraft_MainActivity_nativeCleanUp(JNIEnv *env, jobject obj) {
+        // empty for now, free mem
+    }
 }
--- app/src/main/java/com/chyves/opencraft/MainActivity.kt
+++ app/src/main/java/com/chyves/opencraft/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.chyves.opencraft
 
 import android.os.Bundle
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.view.MotionEvent
+import android.view.KeyEvent
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+import android.opengl.GLSurfaceView
+import android.opengl.GLES20
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +20,49 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val glSurfaceView = GLSurfaceView(this)
+
+        glSurfaceView.layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
+        glSurfaceView.setEGLContextClientVersion(2)
+        glSurfaceView.setRenderer(KotlinBindingRenderer())
+        setContentView(glSurfaceView)
     }
 
-    // get c++ funcs and add here
-    external fun stringFromJNI(): String
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        handleKeyEvent(keyCode, event?.unicodeChar?.toChar().toString())
+        return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event?.let {
+            handleTouchEvent(it.x, it.y, it.action)
+        }
+        return super.onTouchEvent(event)
+    }
+
+    external fun handleTouchEvent(x: Float, y: Float, action: Int)
+    external fun handleKeyEvent(keyCode: Int, keyChar: String)
+}
+
+class KotlinBindingRenderer : GLSurfaceView.Renderer {
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        nativeInitOpenGL()
+    }
+
+    override fun onDrawFrame(gl: GL10?) {
+        nativeRender()
+    }
+
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        GLES20.glViewport(0, 0, width, height)
+    }
+
+    override fun onSurfaceDestroyed() {
+        nativeCleanUp()
+    }
+
+    external fun nativeInitOpenGL()
+    external fun nativeRender()
+    external fun nativeCleanUp()
 }
